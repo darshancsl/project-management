a82d092753d23cf52c082ef0ba38e852ba6df1ec
Coding Scale <codingscale157@gmail.com>
2023-10-18 19:51:51 +0530

    added render uri

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ const cache = new InMemoryCache({
 });
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql",
+  uri: "https://project-management-16n2.onrender.com/graphql",
   cache,
 });
 
